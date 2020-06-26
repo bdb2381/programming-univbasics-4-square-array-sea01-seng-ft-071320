710ce188974f4b488c00bc8c28c6e9110ca382af
Brad Beggs <brad.beggs@gmail.com>
2020-06-26 21:32:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,16 +2,11 @@ def square_array(array)
   # your code here
 counter = 0
 new_array = []
-puts array
-puts "array above........"
-puts "new array below........"
+
 while counter < array.size do
-  #holding_var = array[counter]
-  #new_array << holding_var**2
   new_array << array[counter]**2
   counter += 1
 end
-  puts new_array
-  puts "end method........"
+
 return new_array
 end
