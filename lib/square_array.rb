447b65bae141575489f6597c07c0baa44218b1bf
Brad Beggs <brad.beggs@gmail.com>
2020-06-26 21:20:41 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,8 +5,7 @@ new_array = []
 puts array
 puts "........"
 while counter < array.size do
-  new_array << array["counter"]**
-  #puts array["counter"]
+  new_array << array[counter]**
   counter+=1
   #puts new_array
 end
